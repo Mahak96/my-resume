1454818e0d1f25955ba989c2ce6998b007fce6c9
Mahak <mahakgoyal9880@gmail.com>
2025-09-06 08:54:54 +0530

    Updated Resume project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -12,7 +12,7 @@ function projects() {
         <span>February 2025</span>
       
       </div>
- <p className="std">
+ <div className="std">
             <ul>
             <li> Analyzed 2 million+ COVID-19 cases across 50+ countries to identify trends.</li>
             <li> Engineered Tableau dashboards with 10+ interactive visualizations, tracking case trends.</li>
@@ -20,7 +20,7 @@ function projects() {
             <li> Developed heatmaps and correlation plots revealing 3 major infection trends.</li>
             <li> Recommended policy changes to health organizations based on data insights.</li>
             </ul>
-          </p>
+          </div>
       <div className="edu-item">
         <div>
           <strong>IPL Data Analysis Project </strong>  | Python, Pandas, Matplotlib, Power BI        
@@ -30,7 +30,7 @@ function projects() {
         <span>February 2025</span>
       
       </div>
- <p className="std">
+ <div className="std">
             <ul>
             <li>  Evaluated 700+ IPL matches across 15 seasons to derive player and team statistics..</li>
             <li> Recognized Virat Kohli as the highest run-scorer with 6,000+ runs and Suresh Raina with 500+ fours.</li>
@@ -39,7 +39,7 @@ function projects() {
  enhancing strategic decision-making.</li>
             <li> Created Power BI dashboards with 8+ key metrics to compare team performances.</li>
             </ul>
-          </p>
+          </div>
     </section>
   );
 }
